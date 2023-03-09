6829f5ac198a5dab79bbab4823e93d992ae87383
thulson <thulson@asu.edu>
2023-03-09 00:06:12 -0700

    Set up ros2 ws packaged turtlesim

---
+++ ros2_ws/build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tyler/Documents/Github/280-M5_ROS/ros2_ws/build/turtlesim/turtlesim__py
+# Build directory: /home/tyler/Documents/Github/280-M5_ROS/ros2_ws/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
