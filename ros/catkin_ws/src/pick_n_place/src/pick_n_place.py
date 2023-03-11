f74c84b96f05959f02447fb4f5071cacb0ce07c9
thulsonASU <thulson@asu.edu>
2023-03-11 03:14:45 -0700

    Day 6/7 Days have no meaning

--- ros/catkin_ws/src/pick_n_place/src/pick_n_place.py
+++ ros/catkin_ws/src/pick_n_place/src/pick_n_place.py
@@ -3,9 +3,24 @@
 import rospy, roslib, sys
 import moveit_commander
 import geometry_msgs.msg
+from mycobot_communication.srv import GripperStatus
 
 moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('Pick_n_Place_my280M5')
+
+# Connect to Gripper Service... I think
+
+rospy.init_node("simple_gripper")
+
+rospy.wait_for_service("switch_gripper_status")
+try:
+    switch_gripper = rospy.ServiceProxy(
+        "switch_gripper_status", GripperStatus
+    )
+except:
+    print("start error ...")
+    exit(1)
+
+print("Connect service success.")
 
 robot = moveit_commander.RobotCommander()
 arm = moveit_commander.MoveGroupCommander("arm_group")
@@ -26,19 +41,15 @@ rospy.sleep(2)
 print("= Testing Gripper =")
 rospy.sleep(1)
 
-grip = moveit_commander.MoveGroupCommander("gripper")
-print(grip)
 # Open Gripper
-grip.set_named_target("open")
-grip.go()
 print("Open")
+switch_gripper(True)
 rospy.sleep(2)
-grip.set_named_target("close")
-grip.go()
+# Close Gripper
 print("close")
+switch_gripper(False)
 rospy.sleep(1)
 print("done")
-
 
 rospy.sleep(5)
 moveit_commander.roscpp_shutdown()
